4b998ddd927e0d2f62c8b69a8b5f51885e9c43d5
yabin <yabin.monroy@gmail.com>
2016-02-13 17:18:18 -0600

    hacking

--- src/scripts/market/app.hackingzone.js
+++ src/scripts/market/app.hackingzone.js
@@ -34,24 +34,7 @@ angular.module("app.hackingzone" , [])
           }
         };
         
-        $scope.addnewresource = function(newresource) {
-            console.log(newresource);
-            console.log($scope.resource);
-            if(UserFacebookID.user.id) {
-
-              newresource.user_owner = UserFacebookID.user.id;
-              newresource.id_project = UserFacebookID.project_id;
-              $http.post('/resourcelist', newresource).success(function(response) {
-                console.log(response);
-                if($scope.modalInstance)
-                {
-                  $scope.modalInstance.close();
-                }
-                refreshResourceList();
-              });
-            }
-          };
-        
+       
         $scope.openDialogResource = function() {
 
             $scope.modalInstance = $modal.open({
@@ -64,6 +47,28 @@ angular.module("app.hackingzone" , [])
                 }, function () {
                     console.log('Modal dismissed at: ' + new Date());
                 });
+
+        };
+        
+        $scope.refreshResourceList()t = function() {
+
+            console.log("------refreshResourceList------");
+
+            if(UserFacebookID.user.id) {
+
+              var listParams = {
+                user_owner: UserFacebookID.user.id,
+                project_id: UserFacebookID.project_id
+              };
+              
+              console.log(listParams);
+
+              $http.post('/resourcelistowner/', listParams).success(function(response) {
+                console.log("refresh contributor list");
+                $scope.contributorslist = response;
+                console.log($scope.contributorslist);
+              });
+            }
 
         };
 
@@ -85,16 +90,6 @@ angular.module("app.hackingzone" , [])
               console.log($scope.contributorslist);
             });
           }*/
-
-        };
-
-        $scope.refreshProjectList = function() {
-        		console.log("------refreshProjectList------");
-        		//'UserFacebookID.user.id'
-        		$http.get('/refreshProjectWithUser/' + '123456789').success(function(response) {
-        			console.log("refresh");
-        			$scope.projectlist = response;
-        		});
 
         };
 
