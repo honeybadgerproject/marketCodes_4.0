b5ffc2e78b7d0e3d174d59c6565d5d679c5866e1
yabin <yabin.monroy@gmail.com>
2016-02-15 19:34:41 -0600

    server

--- server.js
+++ server.js
@@ -2,14 +2,77 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var mongojs = require('mongojs');
+var db = mongojs('projectlist', ['projectlist']);
+var dbsrc = mongojs('resourcelist', ['resourcelist']);
+
+
+
 app.use(express.static(__dirname + "/src"));
 app.use(bodyParser.json());
 
 /********  models *********/
 
+/******** projectlist ******/
+/*
+app.get('/refreshProjectWithUser/:id', function(req, res) {
+  var id = req.params.id;
 
-var projectlist = require('./market-codes/projectlist').setProjectList(app);
-//var projectlist = require('./market-codes/').setProjectList(app);
+  db.projectlist.find({$or: [{user_owner:{owner:id, role: "admin"}},{user_owner:{owner:id, role: "super"}}]} , function(err, docs) {
+    res.json(docs);
+  });
+
+});
+
+// create a new project
+app.post('/projectlist', function(req, res) {
+  console.log(req.body);
+  db.projectlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+// delete a project
+app.delete('/projectlist/:id', function(req, res) {
+  var id = req.params.id;
+    console.log(id);
+    db.projectlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+      res.json(doc);
+    });
+});*/
+
+/******* hackingzonelist (resources) **********/
+
+/*app.post('/resourcelistowner', function(req, res) {
+    console.log("request");
+    var id = req.body.user_owner;
+    var id2 = req.body.project_id;
+
+    dbsrc.resourcelist.find( { "user_owner": id , "id_project": id2 } , function(err, docs) {
+      console.log(docs);
+      res.json(docs);
+    });
+});
+
+app.post('/resourcelist', function(req, res) {
+  console.log(req.body);
+  dbsrc.resourcelist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/resourcelist/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(id);
+    dbsrc.resourcelist.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+      res.json(doc);
+    });
+ });*/
+
+
+
+var projectlist = require('./market-codes/projectlist')(app, db);
+var hackinglist = require('./market-codes/hackingzone')(app, dbsrc);
 
 
 /**************************/
