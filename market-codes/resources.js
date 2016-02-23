ab802e7c15336e85fc637fed6b00930c92b44853
yabin <yabin.monroy@gmail.com>
2016-02-22 20:41:49 -0600

    login

---
+++ market-codes/resources.js
@@ -0,0 +1,29 @@
+exports.setResources = function(app, dbsrc) {
+
+  app.post('/resourcelistowner', function(req, res) {
+    console.log("request");
+    var id = req.body.user_owner;
+    var id2 = req.body.project_id;
+
+    dbsrc.resourcelist.find( { "user_owner": id , "id_project": id2 } , function(err, docs) {
+      console.log(docs);
+      res.json(docs);
+    });
+  });
+
+  app.post('/resourcelist', function(req, res) {
+    console.log(req.body);
+    dbsrc.resourcelist.insert(req.body, function(err, doc) {
+      res.json(doc);
+    });
+  });
+
+  app.delete('/resourcelist/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(id);
+    dbsrc.resourcelist.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+      res.json(doc);
+    });
+  });
+
+ };
