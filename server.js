0b8deb2de80e9133c129af9ed83308e22d4cf970
honeybadgerproject <yabin.monroy@gmail.com>
2016-04-05 13:33:53 -0500

    hacker

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var mongojs = require('mongojs');
 var db = mongojs('projectlist', ['projectlist']);
 var dbsrc = mongojs('resourcelist', ['resourcelist']);
+var dbbuddy = mongojs('contributorlist', ['contributorlist']);
 var dbtab = mongojs('notelist', ['notelist']);
 var dbtabprivate = mongojs('notelistprivate', ['notelistprivate']);
 
@@ -22,7 +23,7 @@ var userinfo = require('./market-codes/userinfo').setUserInfo(app, dbuserinfo);
 
 var projectlist = require('./market-codes/projectlist').setProjectList(app, db);
 var resources = require('./market-codes/resources').setResources(app, dbsrc);
-var resources = require('./market-codes/contributor').setContributor(app, db);
+var resources = require('./market-codes/contributor').setContributor(app, dbbuddy);
 var tabs = require('./market-codes/tablist').setTabs(app, dbtab);
 var privatetabs = require('./market-codes/privatetablist').setPrivateTabs(app, dbtabprivate);
 
